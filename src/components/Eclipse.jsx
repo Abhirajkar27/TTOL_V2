8789e6626536a93037a016ed73f09b643246c641
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-07-01 16:34:02 +0530

    new Gamw

---
+++ src/components/Eclipse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Eclipse = (props) => {
+    return (
+        <svg className ={`${props.class}`} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 5 4" fill="none">
+            <ellipse cx="2.32981" cy="1.9613" rx="1.97606" ry="1.77563" transform="rotate(48.2745 2.32981 1.9613)" fill="white" />
+        </svg>
+    )
+}
+
+export default Eclipse;
