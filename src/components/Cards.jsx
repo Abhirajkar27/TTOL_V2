1a6c62495eb9421cc15909a86c4b9c3504b38f42
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-07-01 18:17:33 +0530

    landing page

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cards = ({ color = 'var(--Accent-Accent--04-Accent-04, #36FFA1)', sign = '?', className }) => {
   return (
-    <div className={`Landing_cards ${className}`} style={{ background: color }}>
+    <div className={`Landing_card ${className}`} style={{ background: color }}>
         <p className='Landing_card_sign'>{sign}</p>
     </div>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,15 +3,15 @@ import Cards from '../components/Cards'
 const Landing = () => {
   return (
     <div>
-        <div className="Landing_Upperdiv">
+      <div className="Landing_Upperdiv">
         <p className='Landing_YB'>You both will love</p>
         <p className='Landing_TL'> 2 Truths and a lie</p>
-        </div>
-    
-      <Cards  sign='?' className={'card1'}/>
-      <Cards sign='?'  className={'card2'}/>
-      <Cards sign='!' className={'card3'} color='var(--Accent-Accent--02-Accent-02-neon, #FF5AF9)'/>
-
+      </div>
+      <div className='landing_cards'>
+        <Cards sign='?' className={'card1'} />
+        <Cards sign='?' className={'card2'} />
+        <Cards sign='!' className={'card3'} color='var(--Accent-Accent--02-Accent-02-neon, #FF5AF9)' />
+      </div>
       <div className="activity_info">
         <p className='Landing_activity_info'>Activity info</p>
       </div>
