f571b40e3e26232bd464702feb6a1e604c91cca0
Sagar <sagar.kapoor@vyld.io>
2024-07-01 17:57:34 +0530

    done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,13 +4,21 @@ const Landing = () => {
   return (
     <div>
         <div className="Landing_Upperdiv">
-        <p className='Landing_YB'>You Both will Love</p>
-        <p className='Landing_TL'> 2 Truths and a Lie</p>
+        <p className='Landing_YB'>You both will love</p>
+        <p className='Landing_TL'> 2 Truths and a lie</p>
         </div>
     
       <Cards  sign='?' className={'card1'}/>
       <Cards sign='?'  className={'card2'}/>
       <Cards sign='!' className={'card3'} color='var(--Accent-Accent--02-Accent-02-neon, #FF5AF9)'/>
+
+      <div className="activity_info">
+        <p className='Landing_activity_info'>Activity info</p>
+      </div>
+      <div className="Landing_buttons">
+        <button className='Landing_start'>Start</button>
+        <button className='Landing_activity'>choose another activity</button>
+      </div>
     </div>
   )
 }
