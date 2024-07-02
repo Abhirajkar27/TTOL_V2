ca74081dd50f95e8ff60eeca4a988fffb8613009
Sagar <sagar.kapoor@vyld.io>
2024-07-02 18:02:44 +0530

    done

--- src/components/Truth_Options.jsx
+++ src/components/Truth_Options.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Truth_Options = ({text}) => {
+const Truth_Options = ({ text, onClick }) => {
   return (
-    <div className='Truth_Options'>
-        <div className="textoption">
-      {text}
+    <div className='Truth_Options' onClick={onClick}>
+      <div className="textoption">
+        {text}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Truth_Options
+export default Truth_Options;
