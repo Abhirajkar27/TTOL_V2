30f761ebdfb8c62c7b37316bf295b51c890e35fb
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-07-02 13:26:02 +0530

    done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,7 +10,7 @@ const Landing = () => {
         <p className='Landing_YB'>You both will love</p>
         <p className='Landing_TL'> 2 Truths and a lie</p>
       </div>
-      <div className={`landing_cards ${isShowActivityInfo?'margintop':''}`}>
+      <div className={`landing_cards ${isShowActivityInfo?'lanmargintop':''}`}>
         <Cards sign='?' className={'card1'} />
         <Cards sign='?' className={'card2'} />
         <Cards sign='!' className={'card3'} color='var(--Accent-Accent--02-Accent-02-neon, #FF5AF9)' />
@@ -19,7 +19,7 @@ const Landing = () => {
       {!isShowActivityInfo && <div className="activity_info">
         <p className='Landing_activity_info' onClick={() => { setISShowActivityInfo(true); }}>Activity info</p>
       </div>}
-      <div className="Landing_buttons">
+      <div className={`Landing_buttons ${isShowActivityInfo?'lbmargintop':''}`}>
         <button className='Landing_start'>Start</button>
         <button className='Landing_activity'>choose another activity</button>
       </div>
